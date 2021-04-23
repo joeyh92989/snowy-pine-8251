20de224f000c4775a0828ef9501741cc126dd3c8
Joey Haas <josephthaas@gmail.com>
2021-04-23 08:55:55 -0600

    completed user story 1

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,26 +1,6 @@
-class FlightsController < ApplicationController
-  
+class FlightsController < ApplicationController 
     def index
-      @flights = Flights.all
-    end
-  
-    def show
-      @ = .find(params[:id])
-    end
-  
-    def create
-      @ = .new(params[:])
-    end
-  
-    def update
-      @ = .find(params[:id])
-    end
-  
-    def edit
-      @ = .find(params[:id])
-    end
-  
-    def destroy
-       = .find(params[:id])
+
+      @flights = Flight.all
     end
 end
