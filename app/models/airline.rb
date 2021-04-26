d2b0de215e2818dbbe8a10972ad687f96c381610
Joey Haas <josephthaas@gmail.com>
2021-04-26 15:26:35 -0600

    replaced uniq with distinct within passenger method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,6 +1,6 @@
 class Airline < ApplicationRecord
   has_many :flights
   def passengers
-    flights.joins(:passengers).where('passengers.age >= 18').select('passengers.*').uniq.pluck(:name)
+    flights.joins(:passengers).where('passengers.age >= 18').select('passengers.*').distinct.pluck(:name)
   end
 end
